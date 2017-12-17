9ca02718bd1212f8dcd130b17f09da6b959f53e2
salty_vanilla <jacketgardensio@gmail.com>
2017-12-17 10:29:35 +0900

    add flip

--- image_sampler.py
+++ image_sampler.py
@@ -5,8 +5,11 @@ from PIL import Image
 
 
 class ImageSampler:
-    def __init__(self, image_dir, target_size=None, color_mode='rgb', normalize_mode='tanh', nb_sample=None):
+    def __init__(self, image_dir,
+                 is_flip=True, target_size=None,
+                 color_mode='rgb', normalize_mode='tanh', nb_sample=None):
         self.image_dir = image_dir
+        self.is_flip = is_flip
         self.target_size = target_size
         self.color_mode = color_mode
         self.image_paths = np.array(sorted([path for path in get_image_paths(image_dir)]))
@@ -16,14 +19,16 @@ class ImageSampler:
         self.nb_sample = len(self.image_paths)
 
     def flow(self, batch_size, shuffle=True, seed=None):
-        return DataIterator(paths=self.image_paths, target_size=self.target_size,
+        return DataIterator(paths=self.image_paths,
+                            is_flip=self.is_flip, target_size=self.target_size,
                             color_mode=self.color_mode, batch_size=batch_size,
                             normalize_mode=self.normalize_mode, shuffle=shuffle, seed=seed)
 
 
 class DataIterator(Iterator):
-    def __init__(self, paths, target_size, color_mode, batch_size, normalize_mode, shuffle, seed):
+    def __init__(self, paths, is_flip, target_size, color_mode, batch_size, normalize_mode, shuffle, seed):
         self.paths = paths
+        self.is_flip = is_flip
         self.target_size = target_size
         self.color_mode = color_mode
         self.nb_sample = len(self.paths)
@@ -35,7 +40,7 @@ class DataIterator(Iterator):
         with self.lock:
             index_array, current_index, current_batch_size = next(self.index_generator)
         image_path_batch = self.paths[index_array]
-        image_batch = np.array([load_image(path, self.target_size, self.color_mode)
+        image_batch = np.array([load_image(path, self.is_flip, self.target_size, self.color_mode)
                                 for path in image_path_batch])
         self.current_paths = image_path_batch
         return image_batch
@@ -44,7 +49,7 @@ class DataIterator(Iterator):
         return denormalize(x, self.normalize_mode)
 
 
-def load_image(path, target_size=None, color_mode='rgb'):
+def load_image(path, is_flip=True, target_size=None, color_mode='rgb'):
     assert color_mode in ['grayscale', 'gray', 'rgb']
     image = Image.open(path)
 
@@ -56,6 +61,16 @@ def load_image(path, target_size=None, color_mode='rgb'):
 
     image_array = np.asarray(image)
     image_array = normalize(image_array)
+
+    if len(image_array.shape) == 2:
+        image_array = np.expand_dims(image_array, axis=-1)
+
+    #
+    if is_flip:
+        if np.random.uniform() < 0.5:
+            image_array = image_array[:, ::-1, :]
+        else:
+            pass
     return image_array
 
 
